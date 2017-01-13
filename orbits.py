c382512ffb4fc1b14f1ab6c3e12a263ba98c712d
ndiamant <ndiamant@hmc.edu>
2017-01-12 23:08:25 -0800

    Motion laws now act on moon objects instead of being passed to objects

--- orbits.py
+++ orbits.py
@@ -7,8 +7,8 @@ pygame.init()
 width = 1700
 height = 1000
 v_i = 200
-num = 1000 
-dt = 50
+num = 10 
+dt = 1
 
 surface = pygame.display.set_mode((width, height), 0, 32)
 
@@ -21,15 +21,12 @@ def uncenter(pos):
     return  pos - np.array([ width / 2, height / 2])
 
 def step(moon_list, motion_law):
-    #surface.fill((0,0,0))
 
     for moon in moon_list:
-        moon.step(motion_law)
+        motion_law(moon) 
         moon.draw(surface)
     
     pygame.display.update()
-    #pygame.time.wait(dt)
-
 
 class Moon:
     def __init__(self, pos, color, size, vel):
@@ -41,11 +38,11 @@ class Moon:
     def draw(self, window):
         pygame.draw.circle(window, self.color, (int(self.pos[0]), int(self.pos[1])), self.size, 0)
 
-    def step(self, motion_law):
-        motion_law(self.pos, self.vel)
-
+# Moon list
 ml = [Moon(pos = np.array([np.random.rand() * width, np.random.rand() * height]), color = np.random.randint(0,255,3), size = np.random.randint(2,5), vel = np.random.randn(2) * v_i) for _ in range(num)]
 
+# Take 2x1 np array and cap magnitude to lim
+# TODO: make this description true
 def max_lim(vel, lim = 10000):
     signs = vel / np.abs(vel)
     vel = np.abs(vel)
@@ -53,23 +50,32 @@ def max_lim(vel, lim = 10000):
     vel *= signs
     return vel
 
-def gravity(pos, vel):
+# An example of a motion law
+def gravity(moon):
+    pos = moon.pos
+    vel = moon.vel 
     acc = -100000000 * uncenter(pos) / np.linalg.norm(uncenter(pos))**3
     acc = max_lim(acc)
     vel += acc * dt / 1000
     pos += vel * dt / 1000
 
-def linear(pos, vel):
+def linear(moon):
+    pos = moon.pos
+    vel = moon.vel
     acc = -100000 * uncenter(pos) / np.linalg.norm(uncenter(pos))**2
     acc = max_lim(acc)
     vel += acc * dt / 1000
     pos += vel * dt / 1000
 
 
-def zero(pos, vel):
+def zero(moon):
+    pos = moon.pos
+    vel = moon.vel
     acc = -1000 * uncenter(pos) / np.linalg.norm(uncenter(pos))
     vel += acc * dt / 1000
     pos += vel * dt / 1000
+
+
 while True:
     
     step(ml,linear)   
